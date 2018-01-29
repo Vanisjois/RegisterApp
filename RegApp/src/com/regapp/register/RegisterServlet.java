1913832a25f33dd440fbf361212cccab31a55846
Vani <vani.somlapura@evry.com>
2018-01-29 17:27:09 +0530

    set up register app

---
+++ RegApp/src/com/regapp/register/RegisterServlet.java
@@ -0,0 +1,44 @@
+package com.regapp.register;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        System.out.println("In-register servlet's no arg constructor");
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("In-register servlet's doGet method");
+		String email = request.getParameter("email");
+		String pwd = request.getParameter("pwd");
+		PrintWriter pw = response.getWriter();
+		pw.write("<html><body>Hello "+email+", your registration is succesfull</body></html>");    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("In-register servlet's doPost method");
+	}
+
+}
